17f9b99a600cc679a88d9bb9b2b8a9966ed13293
mosako <64979830+mosako@users.noreply.github.com>
2021-01-10 17:53:53 +0330

    setting inputs

--- suduko.py
+++ suduko.py
@@ -1,3 +1,21 @@
 from tkinter import *
 import tkinter.messagebox
 from tkinter import messagebox
+
+
+root=Tk()
+
+root.geometry('500x500')
+root.resizable(True,True)
+root.title("suduku!!!")
+
+ent=[]
+
+for i in range(1,10):
+    for j in range (1,10):
+        ent.append(Entry(root, width=3))
+        ent[len(ent)-1].grid(row=i, column=j)
+
+
+
+root.mainloop()
