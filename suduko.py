0dcf7cfeeba817546ed975e3c9fd2743a4d37949
mosako <64979830+mosako@users.noreply.github.com>
2021-01-10 21:30:20 +0330

    input system update
    inp[] will contain answers

--- suduko.py
+++ suduko.py
@@ -17,8 +17,21 @@ for i in range(1,10):
         ent[len(ent)-1].grid(row=i, column=j)
 
 
+inp=[]
+
+def catcher():
+    for i in range(9):
+        for j in range (9):
+            inp.append(ent[i*9+j].get())
+
+
+
+def block_check():
+    catcher()
+
+
 buttonFont = font.Font(family='Helvetica', size=10, weight='bold')
-btn_submit = Button(root, text = 'Finish',font=buttonFont, fg = "red", bg = 'brown', width=30)
+btn_submit = Button(root, text = 'Finish',font=buttonFont, fg = "red", bg = 'brown', width=30, command=block_check())
 btn_submit.grid(row = 10, column = 1, columnspan=9)
 
 root.mainloop()
